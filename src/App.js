f77437d514eef0bc11aacd42821ad9e00dcd1399
Mukul Ramchandani <mukul@zineone.com>
2019-07-01 02:33:22 +0530

    Structure Ready

--- src/App.js
+++ src/App.js
@@ -1,40 +1,71 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Container,Row,Col } from 'reactstrap';
 
-function App() {
+export default class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  render(){
   return (
     <div>
       <div>
-  <div className="backgroundMajor">
-{
-  
-  /* <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-  </div> */
-  }
-<div className="backgroundClip"> </div>
-</div>
-<div className="nameInfo">Hey, I'm Mukul</div>
+        <div className="backgroundMajor">
 
-</div>
-</div>
+          <div className="backgroundClip"> </div>
+        </div>
+        <div className="nameInfo">Hey, I'm Mukul</div>
+        <div className="introInfo">
+        
+        <Container>
+          <Row>
+          <Col sm="6">add Who you are ? </Col>
+          <Col sm="6">add What you do currently ?</Col>
+          </Row>
+        </Container>
+
+        </div>
+
+        <div className="bottomNav">
+          <Container>
+        <Row>
+          <Col>Skills</Col>
+          <Col>TimeLine</Col>
+          <Col>Interests</Col>
+          <Col>Contact</Col>
+        </Row>
+         </Container>
+        </div>
+        
+      </div>
+      <div className="skillsInfo">
+<div className="skillsTitle">Skills</div>
+<div className="skillsDetails">Details About Skills</div>
+
+      </div>
+
+      <div className="skillsInfo">
+<div className="skillsTitle">TimeLine</div>
+<div className="skillsDetails">Create a TimeLine Component here...</div>
+
+      </div>
+      <div className="skillsInfo">
+<div className="skillsTitle">Interests</div>
+<div className="skillsDetails">Add some of your hobbies , the books you have read or some future plans you have got.</div>
+
+      </div>
+
+      <div className="skillsInfo">
+<div className="skillsTitle">Contact</div>
+<div className="skillsDetails">Say Hi , let's discuss the idea you have got :)</div>
+
+      </div>
+    </div>
 
   
   );
+  }
 }
-
-export default App;
